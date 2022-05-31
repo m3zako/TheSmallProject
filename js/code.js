7db90302ed15a54419ab605bdff71559615bdb9f
williamsea0208 <105892634+williamsea0208@users.noreply.github.com>
2022-05-31 16:50:21 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -7,9 +7,62 @@ let lastName = "";
 
 function doLogin()
 {
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+	
+	document.getElementById("loginResult").innerHTML = "";
+	
+	let tmp = {login:login,password:password};
+	let jsonPayload = JSON.stringify ( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+				
+				if( userId < 1 )
+				{
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+				
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
+				
+				saveCookie();
+				
+				window.location.href = "contacts.html";
+			}
+		}
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
 }
 
 function doRegister()
+{
+}
+
+function searchContact()
+{
+}
+
+function addContact()
 {
 }
 
