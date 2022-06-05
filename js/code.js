4142bc6a16063e8adf9bcc6e8ca57770eb9cde03
williamsea0208 <105892634+williamsea0208@users.noreply.github.com>
2022-06-05 13:34:35 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -57,13 +57,111 @@ function doLogin()
 
 function doRegister()
 {
+	let newFirstName = document.getElementById("registerFirst").value;
+	let newLastName = document.getElementById("registerLast").value;
+	let newLogin = document.getElementById("registerName").value;
+	let newPassword = document.getElementById("registerPassword").value;
+	document.getElementById("registerResult").innerHTML = "";
+	
+	let tmp = {FirstName:newFirstName,LastName:newLastName,Login:newLogin,Password:newPassword};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Register.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("registerResult").innerHTML = "Registration successful! Please login.";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("registerResult").innerHTML = err.message;
+	}
 }
 
 function searchContact()
 {
+	// let contactList = "<p id=\"contactNameText\">Contact Name</p><p id=\"contactPhoneText\">123-456-7890</p><p id=\"contactEmailText\">contactname@cop-4331-19.live</p><button type=\"button\" id=\"updateButton\" class=\"buttons\" onclick=\"updateContact(\'contactName\',\'contactPhone\',\'contactEmail\');\"> Update Contact </button><button type=\"button\" id=\"deleteButton\" class=\"buttons\" onclick=\"deleteContact(\'contactName\',\'contactPhone\',\'contactEmail\');\"> Delete Contact </button>";
+	// document.getElementById("contactListDiv").innerHTML = contactList;
+
+	let srch = document.getElementById("searchBar").value;
+	document.getElementById("contactSearchResult").innerHTML = "";
+	
+	let contactList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/SearchContact.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					contactList += "<p id=\"contactNameText\">";
+					contactList += jsonObject.results[i].Name;
+					contactList += "</p><p id=\"contactPhoneText\">";
+					contactList += jsonObject.results[i].Phone;
+					contactList += "</p><p id=\"contactEmailText\">";
+					contactList += jsonObject.results[i].Email;
+					contactList += "</p><button type=\"button\" id=\"updateButton\" class=\"buttons\" onclick=\"updateContact(\'";
+					contactList += jsonObject.results[i].Name;
+					contactList += "\',\'";
+					contactList += jsonObject.results[i].Phone;
+					contactList += "\',\'";
+					contactList += jsonObject.results[i].Email;
+					contactList += "\');\"> Update Contact </button><button type=\"button\" id=\"deleteButton\" class=\"buttons\" onclick=\"deleteContact(\'";
+					contactList += jsonObject.results[i].Name;
+					contactList += "\',\'";
+					contactList += jsonObject.results[i].Phone;
+					contactList += "\',\'";
+					contactList += jsonObject.results[i].Email;
+					contactList += "\');\"> Delete Contact </button>";
+					if( i < jsonObject.results.length - 1 )
+					{
+						contactList += "<hr>";
+					}
+				}
+				
+				document.getElementById("contactListDiv").innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
 }
 
 function addContact()
+{
+}
+
+function deleteContact(delName, delPhone, delEmail)
+{
+}
+
+function updateContact(upName, upPhone, upEmail)
 {
 }
 
